3bca4dec5ad25c86a1aa82d54d6afebcea6d672d
Ananya Yadav <ananya.ydvv@gmail.com>
2026-07-12 22:26:26 +0530

    commit

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,39 +1,17 @@
-const {
-  CreateUser,
-  GetAllUsers,
-  DeleteUser,
-  LoginUser
+const {CreateUser, GetAllUsers ,
+    DeleteUser, LoginUser
 } = require('../Controller/UserController');
-
 const express = require('express');
 const upload = require('../Midleware/UploadMidleware');
 const { authMiddleware } = require('../Midleware/AuthMilderware');
-
 const router = express.Router();
 
-// Test Route
-router.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Users API is working"
-  });
-});
-
-// Signup
 router.post('/signup', upload.single('image'), CreateUser);
-
-// Login
-router.post('/login', LoginUser);
-
-// Get all users
 router.get('/all', authMiddleware, GetAllUsers);
-
-// Current logged in user
-router.get('/me', authMiddleware, (req, res) => {
-  res.status(200).json({ user: req.user });
-});
-
-// Delete user
 router.delete('/:id', authMiddleware, DeleteUser);
+router.post('/login', LoginUser);
+router.get('/me', authMiddleware, (req, res) => {
+    res.status(200).json({ user: req.user });
+});
 
 module.exports = router;
