1bd8cad231ee5f4dbe3a3af53668d0a7af3a2f07
Ananya Yadav <ananya.ydvv@gmail.com>
2026-07-12 22:18:04 +0530

    Debug database connection

--- Backend/Confiq/db.js
+++ Backend/Confiq/db.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require("sequelize");
 
+console.log("HOST:", process.env.DB_HOST);
+console.log("PORT:", process.env.DB_PORT);
+console.log("DB:", process.env.DB_NAME);
+console.log("USER:", process.env.DB_USER);
+
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     logging: false,
   }
